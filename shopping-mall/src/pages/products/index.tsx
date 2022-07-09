7cf22e35aa90bb2878d118d9ef423fd0a99fc622
clock0419 <kangdongho0419@gmail.com>
2022-07-09 17:40:53 +0900

    [up]price, grid

--- shopping-mall/src/pages/products/index.tsx
+++ shopping-mall/src/pages/products/index.tsx
@@ -1,7 +1,24 @@
-const ProductList = () => (
-    <div>
-        商品LIST
-    </div>
-)
+import { useQuery } from "react-query"
+import ProductItem from "../../components/product/item"
+import { fetcher, QueryKeys } from "../../queryClient"
+import { Product } from "../../types"
+
+const ProductList = () => {
+    const { data } = useQuery<Product[]>(QueryKeys.PRODUCTS, () => fetcher({
+        method: 'GET',
+        path: '/products'
+    }),
+    )
+
+    return (
+        <div>
+            <ul className="products">
+                {data?.map(product=> (
+                    <ProductItem {... product} key={product.id}/>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
 export default ProductList
