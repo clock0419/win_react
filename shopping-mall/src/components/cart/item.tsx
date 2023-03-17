0452d3e533a72c779ecc9a0e6a0f7dbcfeeea4a7
clock0419 <kangdongho0419@gmail.com>
2023-03-17 14:11:12 +0900

    cart_delete

--- shopping-mall/src/components/cart/item.tsx
+++ shopping-mall/src/components/cart/item.tsx
@@ -1,37 +1,73 @@
-import { SyntheticEvent } from "react";
-import { useMutation } from "react-query";
-import { CartType, UPDATE_CART } from "../../graphql/cart";
-import { graphqlFetcher } from "../../queryClient";
+import { SyntheticEvent } from 'react'
+import { useMutation } from 'react-query'
+import { CartType, DELETE_CART, UPDATE_CART } from '../../graphql/cart'
+import { getClient, graphqlFetcher, QueryKeys } from '../../queryClient'
 
-const CartItem = ({
-    id,
-    imageUrl,
-    price,
-    title,
-    amount
-}: CartType) => {
-    const {mutate: updateCart} = useMutation(({id, amount}: {id: string; amount: number}) =>
-        graphqlFetcher(UPDATE_CART, { id, amount}),
-    )
+const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
+  const queryClient = getClient()
+  const { mutate: updateCart } = useMutation(
+    ({ id, amount }: { id: string; amount: number }) => graphqlFetcher(UPDATE_CART, { id, amount }),
+    {
+      onMutate: async ({ id, amount }) => {
+        await queryClient.cancelQueries(QueryKeys.CART)
+        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(QueryKeys.CART)
+        if (!prevCart?.[id]) return prevCart
 
-    const handleUpdateAmount = (e : SyntheticEvent) => {
-        const amount  = Number((e.target as HTMLInputElement).value)
-        updateCart({id, amount})
-    }
+        const newCart = {
+          ...(prevCart || {}),
+          [id]: { ...prevCart[id], amount },
+        }
+        queryClient.setQueryData(QueryKeys.CART, newCart)
+        return prevCart
+      },
+      onSuccess: newValue => {
+        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(QueryKeys.CART)
+        const newCart = {
+          ...(prevCart || {}),
+          [id]: newValue,
+        }
+        queryClient.setQueryData(QueryKeys.CART, newCart)
+      },
+    },
+  )
 
-    return (
+  const { mutate: deleteCart } = useMutation(
+    ({ id }: { id: string }) => graphqlFetcher(DELETE_CART, { id }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(QueryKeys.CART)
+      },
+    },
+  )
+
+  const handleUpdateAmount = (e: SyntheticEvent) => {
+    const amount = Number((e.target as HTMLInputElement).value)
+    if (amount < 1) return
+    updateCart({ id, amount })
+  }
+
+  const handleDeleteItem = () => {
+    deleteCart({ id })
+  }
+
+  return (
     <li className="cart-item">
-        <img src={imageUrl}/>
-        <p className="cart-item__price">{price}</p>
-        <p className="cart-item__title">{title}</p>
-        <input
-            type="number"
-            className="cart-item__amount"
-            value={amount}
-            onChange={handleUpdateAmount}
-        />
+      <input className="cart-item__checkbox" type="checkbox" name="select-item" />
+      <img className="cart-item__image" src={imageUrl} />
+      <p className="cart-item__price">{price}</p>
+      <p className="cart-item__title">{title}</p>
+      <input
+        className="cart-item__amount"
+        type="number"
+        value={amount}
+        min={1}
+        onChange={handleUpdateAmount}
+      />
+      <button className="cart-item__button" type="button" onClick={handleDeleteItem}>
+        삭제
+      </button>
     </li>
-    )
+  )
 }
 
 export default CartItem
--- shopping-mall/src/pages/cart/index.tsx
+++ shopping-mall/src/pages/cart/index.tsx
@@ -1,22 +1,19 @@
-import { useQuery } from "react-query"
-import CartList from "../../components/cart"
-import { CartType, GET_CART } from "../../graphql/cart"
-import { graphqlFetcher, QueryKeys } from "../../queryClient"
+import { useQuery } from 'react-query'
+import CartList from '../../components/cart'
+import { CartType, GET_CART } from '../../graphql/cart'
+import { graphqlFetcher, QueryKeys } from '../../queryClient'
 
 const Cart = () => {
-    const { data } = useQuery(QueryKeys.CART, () => graphqlFetcher(GET_CART),{
-        staleTime: 0,
-        cacheTime: 1000,
+  const { data } = useQuery(QueryKeys.CART, () => graphqlFetcher(GET_CART), {
+    staleTime: 0,
+    cacheTime: 1000,
+  })
 
-    })
+  const cartItems = Object.values(data || {}) as CartType[]
 
-    const cartItems = Object.values(data || {}) as CartType[]
+  if (!cartItems.length) return <div>장바구니가 비었어요</div>
 
-    console.log('11',Object.values(data || {}) as CartType[])
-
-    if(!cartItems.length) return <div>かーとが空いてます。</div>
-
-    return <CartList items={cartItems} />
+  return <CartList items={cartItems} />
 }
 
 export default Cart
